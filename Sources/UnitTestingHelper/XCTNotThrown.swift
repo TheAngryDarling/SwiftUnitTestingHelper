fe15a27ec6bd7d9868cbab25da514075322f7062
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2020-11-17 16:22:02 -0500

    Added onError callback method to XCTNotThrown to provide a callback to process the error that was thrown

--- Sources/UnitTestingHelper/XCTNotThrown.swift
+++ Sources/UnitTestingHelper/XCTNotThrown.swift
@@ -13,15 +13,18 @@ import XCTest
 ///   - message: Message to display on thrown error
 ///   - file: File of code being called
 ///   - line: Line in code being called
+///   - onError: Callback to execute when an error is thrown
 /// - Returns: Returns nil of error thrown or results from block method
 @discardableResult
 public func XCTNotThrown<R>(_ block: @autoclosure () throws -> R,
                             _ message: @autoclosure () -> String = "",
                             file: StaticString = #file,
-                            line: UInt = #line) -> R? {
+                            line: UInt = #line,
+                            onError: (Error) -> Void = { _ in return }) -> R? {
     do {
         return try block()
     } catch {
+        onError(error)
         var msg: String = message()
         if msg.isEmpty {
             msg = "Test failed with the following error:"
